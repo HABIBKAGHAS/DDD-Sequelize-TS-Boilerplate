711255623ba32c4d9d5fe6902f6b0c3585928e40
HABIBKAGHAS <habibkaghas@gmail.com>
2022-07-04 14:19:05 +0300

    Init Commit

---
+++ Models/Example/Example.model.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+class Example extends Model {
+  public Series!: string;
+  public Example!: string;
+
+  // Auto-generated
+  public id!: number;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public createdBy!: String;
+  public updatedBy!: String;
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        Series: DataTypes.STRING,
+        Example: DataTypes.STRING,
+        createdBy: { type: DataTypes.STRING, allowNull: true },
+        createdAt: { type: DataTypes.DATE, allowNull: true },
+        updatedAt: { type: DataTypes.DATE, allowNull: true },
+        updatedBy: { type: DataTypes.STRING, allowNull: true },
+      },
+      {
+        sequelize: sequelize,
+        freezeTableName: true,
+        name: {
+          singular: "Example",
+          plural: "Example",
+        },
+      }
+    );
+  }
+}
+
+export default Example;
