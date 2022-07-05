de7923477e7eb7756a59b2ac699bb950578a3c6c
HABIBKAGHAS <habibkaghas@gmail.com>
2022-07-05 16:51:36 +0300

    update

--- server.ts
+++ server.ts
@@ -1,5 +1,12 @@
+//import all contrllers in here
+//import ExampleController from "Models/Example/ExampleController";
 import App from "./app";
 
-const app = new App([], 10000);
+const app = new App(
+  [
+    //new ExampleController() initializes all controllers
+  ],
+  10000
+);
 
 app.listen();
